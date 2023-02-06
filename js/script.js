5dc5632c3c74aeec07030b98446064b362139604
gvtbble <junkeeeeer@gmail.com>
2023-02-06 16:52:41 +0300

    script Admin or Not

--- js/script.js
+++ js/script.js
@@ -10,3 +10,20 @@ $(document).ready(function() {
 		$('body').toggleClass('lock');
 	});
 });
+
+let login=prompt('Введите логин');
+let password;
+if (login=="Админ"){
+	password=prompt("Введите пароль")
+	if (password=="Я главный"){
+		alert("Здраствуйте");
+	} else if (password==null){
+		alert("Отменено");
+	} else {
+		alert("Неверный пароль");
+	}
+} else if (login==null){
+	alert("Отменено")
+} else {
+	alert("Я вас не знаю")
+}
