7456ccc9cabcec3db1a3b39f1b0cba8ae64839ee
gvtbble <junkeeeeer@gmail.com>
2023-02-07 17:52:47 +0300

    new js opportunity

--- js/script.js
+++ js/script.js
@@ -10,20 +10,63 @@ $(document).ready(function() {
 		$('body').toggleClass('lock');
 	});
 });
-
-let login=prompt('Введите логин');
-let password;
-if (login=="Админ"){
-	password=prompt("Введите пароль")
-	if (password=="Я главный"){
-		alert("Здраствуйте");
-	} else if (password==null){
-		alert("Отменено");
-	} else {
-		alert("Неверный пароль");
-	}
-} else if (login==null){
-	alert("Отменено")
-} else {
-	alert("Я вас не знаю")
+for(let n=2;n%1==0 & n%n==0 & n<10;n++){
+	if (n % 2 !==0) continue;
+	alert(n);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let number=prompt("Введите число");number<100;number=prompt("Try it again")){
+// 	if ((number>100)||(number==null)) break;
+// 	//Повторять цикл, пока ввод неверен
+// }
+
+// let i = 0;
+// while (i < 3) {
+// 	alert( `number ${i++}!` );
+// 	//To get 1,2,3
+// }
+
+// for (let i=2;i<=10;i++){
+// 	if(i%2) continue;
+// 	alert(i);
+// 	//Выведите чётные числа
+// }
+
+// let login=prompt('Введите логин');
+// let password;
+// if (login=="Админ"){
+// 	password=prompt("Введите пароль")
+// 	if (password=="Я главный"){
+// 		alert("Здраствуйте");
+// 	} else if (password==null){
+// 		alert("Отменено");
+// 	} else {
+// 		alert("Неверный пароль");
+// 	}
+// } else if (login==null){
+// 	alert("Отменено")
+// } else {
+// 	alert("Я вас не знаю")
+// 	//Admin
+// }
