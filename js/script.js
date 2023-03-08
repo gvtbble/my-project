74ade18efd0cc62388a0f6acfd5085514e131a75
gvtbble <junkeeeeer@gmail.com>
2023-03-08 18:22:19 +0300

    allhamdulyyah

--- js/script.js
+++ js/script.js
@@ -12,6 +12,20 @@ $(document).ready(function() {
 });
 
 
+// Напишите функцию filterRange(arr, a, b), которая принимает массив arr, ищет элементы 
+// со значениями больше или равными a и меньше или равными b и возвращает результат в виде массива.
+
+// Функция должна возвращать новый массив и не изменять исходный.
+
+// Например:
+
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+alert( filtered ); // 3,1 (совпадающие значения)
+
+alert( arr ); // 5,3,8,1 (без изменений)
 
 
 
@@ -50,6 +64,46 @@ $(document).ready(function() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
+
+// // То есть дефисы удаляются, а все слова после них получают заглавную букву.
+
+// function camelize(str) {
+	
+// 	return  str
+// 	.split("-")
+// 	.map(
+// 		(word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+// 	  )
+// 	.join("");
+// }
+
+// alert(camelize("background-color"));
+
+// // Примеры:
+
+// // camelize("background-color") == 'backgroundColor';
+// // camelize("list-style-image") == 'listStyleImage';
+// // camelize("-webkit-transition") == 'WebkitTransition';
+// // P.S. Подсказка: используйте split, чтобы разбить строку на массив символов,
+// // потом переделайте всё как нужно и методом join соедините обратно.
 
 // Напишите функцию sumInput(), которая:
 // // Просит пользователя ввести значения, используя prompt и сохраняет их в массив.
