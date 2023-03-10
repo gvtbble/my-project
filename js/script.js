7e66774c4668914f0bde3ef970dc8dbb882d2986
gvtbble <junkeeeeer@gmail.com>
2023-03-10 12:01:49 +0300

    array method training

--- js/script.js
+++ js/script.js
@@ -12,20 +12,6 @@ $(document).ready(function() {
 });
 
 
-// Напишите функцию filterRange(arr, a, b), которая принимает массив arr, ищет элементы 
-// со значениями больше или равными a и меньше или равными b и возвращает результат в виде массива.
-
-// Функция должна возвращать новый массив и не изменять исходный.
-
-// Например:
-
-let arr = [5, 3, 8, 1];
-
-let filtered = filterRange(arr, 1, 4);
-
-alert( filtered ); // 3,1 (совпадающие значения)
-
-alert( arr ); // 5,3,8,1 (без изменений)
 
 
 
@@ -62,24 +48,90 @@ alert( arr ); // 5,3,8,1 (без изменений)
 
 
 
+// function getShort(str) {
+// 	return str
+// 	.split(" ")
+// 	.sort(
+// 		(a,b) => a.length - b.length
+// 		)[0];
+// }
+// alert(getShort("study well"));
+
+// function isPalindrome(str) {
+// 	return str === str
+// 	.split("")
+// 	.reverse()
+// 	.join("");
+// }
+// alert(isPalindrome("шалаш"));
+
+
+// function getInitial(str) {
+
+// 	return str
+// 	.split(" ")
+// 	.map(
+// 		(word) =>  word[0]
+// 	)
+// 	.join(".")
+// 	.toUpperCase() + '.' ;
+
+// }
+
+// alert(getInitial("Том Хэнекс"));
 
 
 
 
+// let arr = [6,5,3,1,8,7,2,4];
+// alert(arr);
+
+// function sortArr() {
+
+// 	for(let i = 0; i < arr.length; i++) {
+// 		for( let j=0; j < arr.length; j++) {	
+// 			let val = arr[j + 1]	
+// 			if (arr[j] > arr[j +1 ]) {
+// 				arr[j+1] = arr[j];
+// 				arr[j] = val;
+
+// 		}
+
+// 		}
+
+// 	}
+
+// }
+
+// sortArr();
+// alert(arr);
 
 
 
 
+// // Напишите функцию filterRangeInPlace(arr, a, b), которая принимает массив arr и удаляет из него все значения кроме тех,
+// // которые находятся между a и b. То есть, проверка имеет вид a ≤ arr[i] ≤ b.
 
+// // Функция должна изменять принимаемый массив и ничего не возвращать.
 
+// // Например:
+// function filterRangeInPlace(arr, a, b) {
 
+// 	for (let i = 0; i < arr.length; i++) {
+// 		let val = arr[i];
 
+// 		if (val >= a || val <=b ) {
+// 			arr.splice(i,1);
+// 			i--;
+// 		}
+// 	}
+// }
 
+// let arr = [5, 3, 8, 1];
 
+// filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
 
-
-
-
+// alert( arr ); // [3, 1]
 
 // Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
 
