aa23f3e9f10429274a0e412a08e0f34ec1b99405
gvtbble <junkeeeeer@gmail.com>
2023-03-11 17:48:10 +0300

    xxx

--- js/script.js
+++ js/script.js
@@ -48,6 +48,85 @@ $(document).ready(function() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+// // Создайте функцию конструктор Calculator, которая создаёт «расширяемые» объекты калькулятора.
+
+// // Задание состоит из двух частей.
+
+// // Во-первых, реализуйте метод calculate(str), который принимает строку типа "1 + 2" в формате
+// // «ЧИСЛО оператор ЧИСЛО» (разделено пробелами) и возвращает результат. Метод должен понимать плюс + и минус -.
+
+// // Пример использования:
+// function Calculator() {
+
+// 	this.methods = {
+// 	"-": (a, b) => a - b,
+// 	"+": (a, b) => a + b
+// 	};
+
+// 	this.calculate = function(str) {
+
+// 	let split = str.split(' '),
+// 		a = +split[0],
+// 		op = split[1],
+// 		b = +split[2]
+
+// 	if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+// 		return NaN;
+// 	}
+
+// 	return this.methods[op](a, b);
+// 	}
+
+// 	this.addMethod = function(name, func) {
+// 		this.methods[name] = func;
+// 	};
+// }
+
+// let calc = new Calculator;
+
+// alert( calc.calculate("3 + 7") ); // 10
+
+
+
+// // У нас есть массив строк arr. Нужно получить отсортированную копию, но оставить arr неизменённым.
+
+// // Создайте функцию copySorted(arr), которая будет возвращать такую копию.
+
+// let arr = ["HTML", "JavaScript", "CSS"];
+
+// let copySorted = (arr) => arr.concat().sort() ;
+
+// let sorted = copySorted(arr);
+
+// alert( sorted ); // CSS, HTML, JavaScript
+// alert( arr ); // HTML, JavaScript, CSS (без изменений)
+
+
+// function sumDigits(number) {
+
+// 	return Math.abs(number)
+// 	.toString()
+// 	.split("")
+// 	.reduce(
+// 		(sum,number) =>  +sum + +number 
+// 		)
+
+// }
+// alert(sumDigits(-55));
+
+
 // function getShort(str) {
 // 	return str
 // 	.split(" ")
